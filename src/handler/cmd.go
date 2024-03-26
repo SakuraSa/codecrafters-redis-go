fd1593f1d702b52f48d7d1ba6a342225a4ca3e19
rnd495 <sakura_kyon@hotmail.com>
2024-03-27 02:43:56 +0800

    feat: add get/set

--- src/handler/cmd.go
+++ src/handler/cmd.go
@@ -16,10 +16,13 @@ var (
 )
 
 type CommandHandler struct {
+	Mem map[string][]byte
 }
 
 func NewCommandHandler() *CommandHandler {
-	return &CommandHandler{}
+	return &CommandHandler{
+		Mem: make(map[string][]byte),
+	}
 }
 
 func (h *CommandHandler) HandleConnection(conn net.Conn) error {
@@ -35,7 +38,7 @@ func (h *CommandHandler) HandleConnection(conn net.Conn) error {
 
 		switch cmdAndArgs.Command() {
 		case "ping":
-			if err := h.writeStringResp(conn, "PONG"); err != nil {
+			if err := h.writeSimpleStringResp(conn, "PONG"); err != nil {
 				return fmt.Errorf("error writing response: %v", err)
 			}
 		case "echo":
@@ -47,6 +50,33 @@ func (h *CommandHandler) HandleConnection(conn net.Conn) error {
 			}
 			if err := h.writeBytesResp(conn, cmdAndArgs.Args[1]); err != nil {
 				return fmt.Errorf("error writing response: %v", err)
+			}
+		case "set":
+			if len(cmdAndArgs.Args) != 3 {
+				if err := h.writeErrorResp(conn, fmt.Sprintf("set requires 3 argument, %v\r\n", cmdAndArgs)); err != nil {
+					return fmt.Errorf("error writing response: %v", err)
+				}
+				continue
+			}
+			h.Mem[string(cmdAndArgs.Args[1])] = cmdAndArgs.Args[2]
+			if err := h.writeSimpleStringResp(conn, "OK"); err != nil {
+				return fmt.Errorf("error writing response: %v", err)
+			}
+		case "get":
+			if len(cmdAndArgs.Args) != 2 {
+				if err := h.writeErrorResp(conn, fmt.Sprintf("get requires 2 argument, %v\r\n", cmdAndArgs)); err != nil {
+					return fmt.Errorf("error writing response: %v", err)
+				}
+				continue
+			}
+			if val, ok := h.Mem[string(cmdAndArgs.Args[1])]; ok {
+				if err := h.writeBytesResp(conn, val); err != nil {
+					return fmt.Errorf("error writing response: %v", err)
+				}
+			} else {
+				if err := h.writeNilResp(conn); err != nil {
+					return fmt.Errorf("error writing response: %v", err)
+				}
 			}
 		default:
 			if _, err := conn.Write([]byte(fmt.Sprintf("-ERR unknown command, %v\r\n", cmdAndArgs))); err != nil {
@@ -73,8 +103,22 @@ func (h *CommandHandler) writeBytesResp(conn net.Conn, resp []byte) error {
 	return nil
 }
 
-func (h *CommandHandler) writeStringResp(conn net.Conn, resp string) error {
-	return h.writeBytesResp(conn, []byte(resp))
+func (h *CommandHandler) writeSimpleStringResp(conn net.Conn, resp string) error {
+	if _, err := conn.Write([]byte("+")); err != nil {
+		return err
+	} else if _, err = conn.Write([]byte(resp)); err != nil {
+		return err
+	} else if _, err = conn.Write([]byte("\r\n")); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *CommandHandler) writeNilResp(conn net.Conn) error {
+	if _, err := conn.Write([]byte("$-1\r\n")); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (h *CommandHandler) writeErrorResp(conn net.Conn, msg string) error {
