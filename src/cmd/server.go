513bd569a7bc0307532e1f0956cb3836d36148da
rnd495 <sakura_kyon@hotmail.com>
2024-03-27 23:59:45 +0800

    feat: mulformed args

--- src/cmd/server.go
+++ src/cmd/server.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
+	"strconv"
 
 	"github.com/codecrafters-io/redis-starter-go/src/handler"
 	"github.com/codecrafters-io/redis-starter-go/src/server"
@@ -14,18 +14,29 @@ import (
 func main() {
 	// Define flags for address and port
 	var (
-		addressFlag = flag.String("address", "0.0.0.0", "address to bind to")
-		portFlag    = flag.Int("port", 6379, "port to bind to")
+		addressFlag   = flag.String("address", "0.0.0.0", "address to bind to")
+		portFlag      = flag.Int("port", 6379, "port to bind to")
+		replicaofFlag = flag.String("replicaof", "", "replicaof address and port")
+		replicaofPort int
 	)
 
 	flag.Parse()
+	if len(*replicaofFlag) > 0 {
+		replicaofPort, _ = strconv.Atoi(flag.Args()[0])
+	}
 
 	ctx := context.Background()
-	log.Default().SetOutput(os.Stdout)
 	log.Printf("Starting server on %s:%d\n", *addressFlag, *portFlag)
 	server := server.NewTCPServer(*addressFlag, *portFlag)
 	redisHandler := handler.NewCommandHandler()
-	redisHandler.Conf.Role = "master"
+	redisHandler.Conf.ReplicaofAddress = *replicaofFlag
+	redisHandler.Conf.ReplicaofPort = replicaofPort
+	if len(redisHandler.Conf.ReplicaofAddress) > 0 && redisHandler.Conf.ReplicaofPort > 0 {
+		redisHandler.Conf.Role = "salve"
+	} else {
+		redisHandler.Conf.Role = "master"
+	}
+	log.Printf("Redis conf: %v\n", &redisHandler.Conf)
 	server.SetHandler(redisHandler)
 
 	if err := server.Loop(ctx); err != nil {
--- src/model/conf.go
+++ src/model/conf.go
@@ -1,5 +1,14 @@
 package model
 
+import "encoding/json"
+
 type CommandConf struct {
-	Role string
+	Role             string
+	ReplicaofAddress string
+	ReplicaofPort    int
+}
+
+func (c CommandConf) String() string {
+	buf, _ := json.Marshal(&c)
+	return string(buf)
 }
